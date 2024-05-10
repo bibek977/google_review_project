0060b72b711a49bcfce788f29bf95df713ff0e71
bibek977 <bhattaraibibek91@gmail.com>
2024-05-10 16:38:40 +0545

    procfile celeary and url.py repath

--- Procfile
+++ Procfile
@@ -1,1 +1,3 @@
 web: gunicorn google_review_project.wsgi:application --log-file - --log-level debug
+worker: celery -A google_review_project worker -l info
+worker2: celery -A google_review_project worker -l info
--- google_review_project/urls.py
+++ google_review_project/urls.py
@@ -3,12 +3,13 @@ from django.urls import path,include,re_path
 from .views import *
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/',include('show_review.api.urls')),
     path('settings/',include('settings_review.api.urls')),
-    path('', index,name='index')
+    re_path(r'^.*', TemplateView.as_view(template_name='index.html')), 
 ]
 
 urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
